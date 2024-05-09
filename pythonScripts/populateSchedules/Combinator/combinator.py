5348b5032f6f67c641733ec2bdc8107a3e1fbf99
AGunnarP <AnthonyGunnarPage@gmail.com>
2024-05-09 15:51:26 -0700

    Combinator combinates all needed courses and concatanates a collection of courses that are equivalent conjuncting concomitant courses

---
+++ pythonScripts/populateSchedules/Combinator/combinator.py
@@ -0,0 +1,51 @@
+class ReturnType:
+    def __init__(self, res, arr) -> None:
+
+        self.res = res
+        self.arr = arr
+
+
+
+def combinate(arr, n, combinations, find):
+    
+    r = n
+    while r > 0:
+        result = findCombination(arr, n, r, find)
+        if result:
+            combinations.append(result.res)
+            return combinate(result.arr, len(result.arr), combinations, find)
+        
+        r-=1
+    return combinations
+
+def findCombination(arr, n ,r, find):
+
+    data = [0] * r
+    return recComb(arr, data, 0, n-1, 0, r, find)
+
+def recComb(arr, data, start, end, index, r, find):
+
+    if index == r:
+        fResult = find(data[:r])
+        return ReturnType(fResult, [x for x in arr if x not in data[:r]]) if fResult else None
+    
+    i=start
+    while i<= end and end-i+1 >= r-index:
+        data[index] = arr[i]
+        result = recComb(arr,data,i+1,end,index + 1, r, find)
+        i+=1
+        if result:
+            return result
+
+
+myArray = [0,1,2,3]
+
+def findAndPrint(thing):
+
+    if thing and len(thing)<3:
+        return thing
+    return None
+
+print(combinate(myArray,4, [], findAndPrint))
+
+
