2f45cd28ddae131082b7cf7b3f406abe35e0f40f
nonetypes <nonetypes@gmail.com>
2024-05-10 17:07:28 -0700

    Comments and docstrings for pupulateSchedules script

--- pythonScripts/populateSchedules/populateSchedules.py
+++ pythonScripts/populateSchedules/populateSchedules.py
@@ -35,12 +35,32 @@ def join_path(file_name: str, dir_levels_up: int = 0) -> str:
     return os.path.join(desired_dir, file_name)
 
 
-def find(courses: list, agreements) -> dict:
+def find(courses: list, agreements: dict) -> dict:
     """
-    coures: list of courses as strings to be matched
+    Finds the equivalent course(s) between institutions.
+
+    Parameters
+    ---
+    courses: list
+        A list of courses as strings to be matched,
+        potentially as an entire group.
+        The course list corresponds to the receiving institution,
+        e.g. Cal Poly.
+
+    agreements: dict
+        An articulation agreement as a dictionary.
+        Expected to be in the exact format as outputted from WebScrape-Assit.
+
+    Returns
+    ---
+    dict
+        The equivalent courses from the sending institution, e.g., a CCC,
+        or None if there is no match found.
     """
     for agreement in agreements["agreements"]:
+        # Ignore OR relationships on the receiving side
         if agreement["receiving"]["conjunction"] in [None, "AND"]:
+            # Find a match on receiving regardless of course ordering
             if set(courses) == set([x["courseNumber"]for x in
                                     agreement["receiving"]["courses"]]):
                 return agreement["sending"]
@@ -49,21 +69,25 @@ def find(courses: list, agreements) -> dict:
 with open(join_path(MAJOR_FILE_NAME), 'r') as file:
     schedule_template = json.load(file)
 
+# Iterate through the list of all community college agreement files
 for agreement_file in os.listdir(join_path(AGREEMENT_PATH, 1)):
 
+    # Open each agreement
     with open(join_path(f"{AGREEMENT_PATH}{agreement_file}", 1), 'r') as file:
         agreement = json.load(file)
 
     ccc_schedule = deepcopy(schedule_template)
-
-    for i, term in enumerate(ccc_schedule["termData"]):
-        courses = [x['id'] for x in term["courses"] if x["id"] is not None]
+    # Swap matched courses into the ccc_schedule
+    for i, term in enumerate(schedule_template["termData"]):
+        courses = [x["id"] for x in term["courses"] if x["id"] is not None]
         custom_courses = [x for x in term["courses"] if x["id"] is None]
         if courses:
             swapped_courses = combinate(courses, len(courses), [],
                                         lambda x: find(x, agreement))
             swapped_courses = swapped_courses + custom_courses
             ccc_schedule["termData"][i]["courses"] = swapped_courses
+
+        # Save the schedule for each community college for the given major
         file_path = join_path(f"json_files/calpolySchedules/COMPUTER_SCIENCE/{file.name.split('/')[-1]}", 1)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w') as file:
