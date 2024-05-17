f9228f3842e605753e187b079a023732a64913b2
Castro19 <ccastroo@calpoly.edu>
2024-05-17 16:50:55 -0700

    ScheduleCard Implemented

--- client/src/components/schedulePage/crudSchedules/scheduleList/ScheduleList.tsx
+++ client/src/components/schedulePage/crudSchedules/scheduleList/ScheduleList.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import ScheduleCard from "./scheduleCard/ScheduleCard";
+const fakeList = [
+  {
+    id: 1,
+    ccc: "San Mateo College",
+    univ: "California Polytechnic University, San Luis Obispo",
+    major: "COMPUTER SCIENCE, B.S.",
+    year: "2024",
+  },
+  {
+    ccc: "Clovis Community College",
+    univ: "California Polytechnic University, San Luis Obispo",
+    major: "COMPUTER SCIENCE, B.S.",
+    year: "2024",
+  },
+];
 
-const ScheduleList = () => {
-  return <div>ScheduleList</div>;
+const ScheduleList = (): JSX.Element => {
+  return (
+    <>
+      {fakeList.map((college, index) => (
+        <div key={index}>
+          <ScheduleCard
+            ccc={college.ccc}
+            univ={college.univ}
+            major={college.major}
+            year={college.year}
+          />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ScheduleList;
--- client/src/pages/savedSchedules/SavedSchedulesPage.tsx
+++ client/src/pages/savedSchedules/SavedSchedulesPage.tsx
@@ -1,6 +1,8 @@
 import ScheduleList from "@/components/schedulePage/crudSchedules/scheduleList/ScheduleList";
-
+import { useLoaderData } from "react-router-dom";
 const SavedSchedulesPage = (): JSX.Element => {
+  const savedSchedules = useLoaderData();
+  console.log("SCHEDULE INS SAVED: ", savedSchedules);
   return (
     <div>
       <p>SavedSchedulesPage</p>
