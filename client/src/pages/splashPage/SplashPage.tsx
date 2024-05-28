cd85b2bfd36ce62b9aab729b9419c3a1847779af
Jake Huey <jakehuey@gmail.com>
2024-05-28 15:34:10 -0700

    magic button

--- client/src/pages/splashPage/SplashPage.tsx
+++ client/src/pages/splashPage/SplashPage.tsx
@@ -21,17 +21,18 @@ const SplashPage = () => {
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          California Community College...
+          California Community College Transfer Helper
         </div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
           And this, is chemical burn.
         </div>
-        <button
-          onClick={goToHomePage}
-          className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
-        >
-          Debug now
-        </button>
+        <button onClick = {goToHomePage} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    Get Started
+  </span>
+</button>
+        
       </motion.div>
     </AuroraBackground>
   );
