3de260c3c897a3373d987e2823da7db13f25f4ff
Castro19 <ccastroo@calpoly.edu>
2024-06-02 08:15:30 -0700

    Update firebase.ts

--- client/src/firebase/firebase.ts
+++ client/src/firebase/firebase.ts
@@ -12,8 +12,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("Firebase Configuration: ", firebaseConfig);
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
